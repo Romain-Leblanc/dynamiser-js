c7f1f5e3519b5a6556e3986c433be94479c1aebf
Romain-Leblanc <romleb2001@gmail.com>
2022-10-22 12:20:36 +0200

    correction génération des dés

--- js/canvas.js
+++ js/canvas.js
@@ -1,127 +1,121 @@
+// Récupère l'élement du DOM pour le canvas
 let canvasDom = document.getElementById("canvas-de");
+// Récupère son contexte
 let ctx = canvasDom.getContext("2d");
 
+// Initialisation les couleurs utiles
+let black = "black";
+let white = "white";
+let red = "red";
+let transparent = "transparent";
+
+// Définit les coordonnées de départ du carré
+let xStart, yStart;
+xStart = yStart = 1;
+
+// Définit les dimensions du carré
+let width, height;
+width = height = 100;
+
+// Définit l'angle final utilisé pour les cercles
 let angleStart = 0;
 let angleEnd = Math.PI * 2;
 
+// Initialise la couleur du carré
 ctx.strokeStyle = "transparent";
+// Initialise la couleur des contours du cercle
 ctx.fillStyle = "red";
 
-// Carré
-ctx.beginPath();
-ctx.rect(1, 1, 100, 100);
-ctx.stroke();
+/**
+ * Génère un carré pour l'affichage des dés
+ */
+ function genererCarre() {
+    ctx.beginPath();
+    ctx.rect(xStart, yStart, width, height);
+    ctx.stroke();
+}
 
-// Dé n°1
+// Génère un arc de cercle avec les paramètres fournis
+ function genererArc(x, y, rayon, angleStart, angleEnd) {
+    ctx.arc(x, y, rayon, angleStart, angleEnd);
+    ctx.stroke();
+    ctx.fill();
+}
+
+// Génère le dé n°1
 function un() {
+    genererCarre();
     ctx.beginPath();
-    ctx.arc(50, 50, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(50, 50, 10, angleStart, angleEnd);
     ctx.closePath();
 }
 
-// Dé n°2
+// Génère le dé n°2
 function deux() {
+    genererCarre();
     ctx.beginPath();
-    ctx.arc(25, 25, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(25, 25, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(75, 75, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(75, 75, 10, angleStart, angleEnd);
     ctx.closePath();
 }
 
-// Dé n°3
+// Génère le dé n°3
 function trois() {
+    genererCarre();
     ctx.beginPath();
-    ctx.arc(25, 25, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(25, 25, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(50, 50, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(50, 50, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(75, 75, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(75, 75, 10, angleStart, angleEnd);
     ctx.closePath();
 }
 
-// Dé n°4
+// Génère le dé n°4
 function quatre() {
+    genererCarre();
     ctx.beginPath();
-    ctx.arc(25, 25, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(25, 25, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(25, 75, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(25, 75, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(75, 25, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(75, 25, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(75, 75, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(75, 75, 10, angleStart, angleEnd);
     ctx.closePath();
 }
 
-// Dé n°5
+// Génère le dé n°5
 function cinq() {
+    genererCarre();
     ctx.beginPath();
-    ctx.arc(25, 25, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(25, 25, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(25, 75, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(25, 75, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(50, 50, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(50, 50, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(75, 25, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(75, 25, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(75, 75, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(75, 75, 10, angleStart, angleEnd);
     ctx.closePath();
 }
 
-// Dé n°6
+// Génère le dé n°6
 function six() {
+    genererCarre();
     ctx.beginPath();
-    ctx.arc(25, 25, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(25, 25, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(25, 50, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(25, 50, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(25, 75, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(25, 75, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(75, 25, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(75, 25, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(75, 50, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(75, 50, 10, angleStart, angleEnd);
     ctx.beginPath();
-    ctx.arc(75, 75, 10, angleStart, angleEnd);
-    ctx.stroke();
-    ctx.fill();
+    genererArc(75, 75, 10, angleStart, angleEnd);
     ctx.closePath();
 }
